f54400b3dc9de33032eba468acb00b6b3a7b350a
iridesuwa18 <iridesuwa18@gmail.com>
2026-08-31 21:01:14 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Bump CACHE_NAME any time you change index.html / app.js / style.css,
    otherwise Android may keep serving the old cached versions even after
    you push new files to GitHub. */
-const CACHE_NAME = "e7-draft-v2";
+const CACHE_NAME = "e7-draft-v3";
 
 const SHELL_FILES = [
   "./index.html",
